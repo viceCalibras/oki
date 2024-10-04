95c5dc852a2037a18ec902ae002818ba7458b312
viceCalibras <vice.roncevic@gmail.com>
2024-10-04 09:24:40 +0200

    Added scale back

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ torch.cuda.empty_cache()
 
 # Set the loss function and optimization algorithm.
 loss_f = torch.nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
 model.to(device)
 trained_model, train_loss, valid_loss = train(
-    model, device, 5000, loss_f, optimizer, train_dataloader, test_dataloader
+    model, device, 1000, loss_f, optimizer, train_dataloader, test_dataloader
 )
 
 save_model_and_loss("./trained_models", model, "new_model", train_loss, valid_loss)
